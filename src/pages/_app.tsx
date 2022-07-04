d4b1c5315c484657a951191bf930fb4958e42194
Vitalii Bondarenko <witcherlin@gmail.com>
2022-07-04 18:40:10 +0300

    Added logging

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import '../styles/globals.css';
 import { AppProps } from 'next/app';
 
 function MyApp({ pageProps, Component }: AppProps) {
+  console.log('MyApp');
   return (
     <Component {...pageProps} />
   );
